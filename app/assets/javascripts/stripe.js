57ea77ddd05d2d7afa57f3be076b8297cec1abff
dobiedad <leomdiv@gmail.com>
2015-06-16 16:44:59 +0100

    display donation_count and total_in_pounds

--- app/assets/javascripts/stripe.js
+++ app/assets/javascripts/stripe.js
@@ -32,3 +32,14 @@ function stripeCheckout() {
     amount:$('input[name=amount]').val() * 100,
   });
 }
+
+function getBalance(){
+  var balance = StripeCheckout.configure({
+    key: $('meta[name="stripe-key"]').attr('content'),
+    token: function(token) {
+      form.find('input[name=stripeToken]').val(token.id);
+      form.find('input[name=amount]').val();
+      form.submit();
+    }
+  });
+}
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,11 @@
+require "stripe"
+
 class HomeController < ApplicationController
+  def index
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    @array_of_donations = Stripe::BalanceTransaction.all["data"].map { |item| item["amount"] }
+    @donation_count =Stripe::BalanceTransaction.all["data"].size
+    @total_in_pounds = (@array_of_donations.reduce :+) / 100
+  end
+
 end
