c08e0ec4e3f74be8e21c4fe8b0ee0ead79f3a3ca
dobiedad <leomdiv@gmail.com>
2015-06-16 17:37:44 +0100

    limit 100

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ require "stripe"
 class HomeController < ApplicationController
   def index
     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-    @array_of_donations = Stripe::BalanceTransaction.all["data"].select{ |item| item["type"] == "charge" }.map { |item| item["amount"] }
+    @array_of_donations = Stripe::BalanceTransaction.all(:limit => 100)["data"].select{ |item| item["type"] == "charge" }.map { |item| item["amount"] }
     @donation_count = @array_of_donations.size
     @total_in_pounds = (@array_of_donations.sum) / 100
 
