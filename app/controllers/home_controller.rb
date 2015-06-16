0042b36cdb047beeb647600de063917bf6dfbe5b
dobiedad <leomdiv@gmail.com>
2015-06-16 17:11:56 +0100

    where type = charge

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,10 @@ require "stripe"
 class HomeController < ApplicationController
   def index
     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-    @array_of_donations = Stripe::BalanceTransaction.all["data"].map { |item| item["amount"] }
+    @array_of_donations = Stripe::BalanceTransaction.all["data"].select{ |item| item["type"] == "charge" }.map { |item| item["amount"] }
     @donation_count =Stripe::BalanceTransaction.all["data"].size
-    @total_in_pounds = (@array_of_donations.reduce :+) / 100
+    @total_in_pounds = (@array_of_donations.sum) / 100
+
   end
 
 end
