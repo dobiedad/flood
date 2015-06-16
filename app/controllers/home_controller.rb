928662208132eb15536365e8c76e9a43f11a82d3
dobiedad <leomdiv@gmail.com>
2015-06-16 17:15:15 +0100

    query for donation_count also

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def index
     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
     @array_of_donations = Stripe::BalanceTransaction.all["data"].select{ |item| item["type"] == "charge" }.map { |item| item["amount"] }
-    @donation_count =Stripe::BalanceTransaction.all["data"].size
+    @donation_count = @array_of_donations.size
     @total_in_pounds = (@array_of_donations.sum) / 100
 
   end
