9cf9aa209b2fc058983ee7464c19da291ee514e6
dobiedad <leomdiv@gmail.com>
2015-06-17 14:44:26 +0100

    fetch_all_transactions_after

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,17 @@ require "stripe"
 class HomeController < ApplicationController
   def index
     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-    @array_of_donations = Stripe::BalanceTransaction.all(:limit => 100)["data"].select{ |item| item["type"] == "charge" }.map { |item| item["amount"] }
+    @array_of_donations = fetch_all_transactions_after(nil).map { |item| item["amount"]}
     @donation_count = @array_of_donations.size
     @total_in_pounds = (@array_of_donations.sum) / 100
-    # if @donation_count = 100
-    #   @starting_after=@array_of_donations.last
-    #   @array_of_donations = Stripe::BalanceTransaction.all(:limit => 100)["data"].select{ |item| item["type"] == "charge" }.map { |item| item["amount"] }
-    # end
+  end
+
+  def fetch_all_transactions_after(t)
+    page = Stripe::BalanceTransaction.all(limit: 100, starting_after: t)["data"].select{ |item| item["type"] == "charge" }
+    if page.last.nil?
+      []
+    else
+      page + fetch_all_transactions_after(page.last)
+    end
   end
 end
